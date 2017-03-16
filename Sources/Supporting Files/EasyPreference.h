c2006b1f8e38c5690b88eeeae4837e84314d9e6f
Xander Deng <dengxiang2010@gmail.com>
2017-03-16 13:15:23 +0800

    Add targets

---
+++ Sources/Supporting Files/EasyPreference.h
@@ -0,0 +1,17 @@
+//
+//  EasyPreference.h
+//  EasyPreference
+//
+//  Created by 邓翔 on 2017/3/16.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EasyPreference.
+FOUNDATION_EXPORT double EasyPreferenceVersionNumber;
+
+//! Project version string for EasyPreference.
+FOUNDATION_EXPORT const unsigned char EasyPreferenceVersionString[];
+
+
