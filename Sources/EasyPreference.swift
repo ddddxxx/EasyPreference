fef4349b5adddc60b5eda7e7121d6a03776f1218
Xander Deng <dengxiang2010@gmail.com>
2017-03-16 13:23:51 +0800

    Add EasyPreference class

---
+++ Sources/EasyPreference.swift
@@ -0,0 +1,98 @@
+//
+//  EasyPreference.swift
+//  EasyPreference
+//
+//  Created by 邓翔 on 2017/3/16.
+//
+//
+
+import Foundation
+
+public class EasyPreference {
+    
+    let defaults: UserDefaults
+    
+    public init(defaults: UserDefaults = .standard) {
+        self.defaults = defaults
+    }
+    
+}
+
+extension EasyPreference {
+    
+    public subscript(_ key: PreferenceKey<String>) -> String? {
+        get { return defaults.string(forKey: key.rawValue) }
+        set { defaults.set(newValue, forKey: key.rawValue) }
+    }
+    
+    public subscript(_ key: PreferenceKey<[Any]>) -> [Any]? {
+        get { return defaults.array(forKey: key.rawValue) }
+        set { defaults.set(newValue, forKey: key.rawValue) }
+    }
+    
+    public subscript(_ key: PreferenceKey<[String: Any]>) -> [String: Any]? {
+        get { return defaults.dictionary(forKey: key.rawValue) }
+        set { defaults.set(newValue, forKey: key.rawValue) }
+    }
+    
+    public subscript(_ key: PreferenceKey<Data>) -> Data? {
+        get { return defaults.data(forKey: key.rawValue) }
+        set { defaults.set(newValue, forKey: key.rawValue) }
+    }
+    
+    public subscript(_ key: PreferenceKey<[String]>) -> [String]? {
+        get { return defaults.stringArray(forKey: key.rawValue) }
+        set { defaults.set(newValue, forKey: key.rawValue) }
+    }
+    
+    public subscript(_ key: PreferenceKey<Int>) -> Int {
+        get { return defaults.integer(forKey: key.rawValue) }
+        set { defaults.set(newValue, forKey: key.rawValue) }
+    }
+    
+    public subscript(_ key: PreferenceKey<Float>) -> Float {
+        get { return defaults.float(forKey: key.rawValue) }
+        set { defaults.set(newValue, forKey: key.rawValue) }
+    }
+    
+    public subscript(_ key: PreferenceKey<Double>) -> Double {
+        get { return defaults.double(forKey: key.rawValue) }
+        set { defaults.set(newValue, forKey: key.rawValue) }
+    }
+    
+    public subscript(_ key: PreferenceKey<Bool>) -> Bool {
+        get { return defaults.bool(forKey: key.rawValue) }
+        set { defaults.set(newValue, forKey: key.rawValue) }
+    }
+    
+    public subscript(_ key: PreferenceKey<URL>) -> URL? {
+        get { return defaults.url(forKey: key.rawValue) }
+        set { defaults.set(newValue, forKey: key.rawValue) }
+    }
+    
+    public subscript(_ key: PreferenceKey<Any>) -> Any? {
+        get { return defaults.object(forKey: key.rawValue) }
+        set { defaults.set(newValue, forKey: key.rawValue) }
+    }
+    
+}
+
+// MARK: - PreferenceKey
+
+public struct PreferenceKey<T>: RawRepresentable, Hashable {
+    
+    public let rawValue: String
+    
+    public init!(rawValue: String) {
+        self.rawValue = rawValue
+    }
+    
+    public init(_ rawValue: String) {
+        self.rawValue = rawValue
+    }
+    
+    public var hashValue: Int {
+        return rawValue.hashValue
+    }
+    
+}
