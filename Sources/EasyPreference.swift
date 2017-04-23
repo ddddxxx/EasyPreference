7ba34bca36c38039f7590947c173ee5107422387
Xander Deng <dengxiang2010@gmail.com>
2017-04-23 18:30:58 +0800

    nest types

--- Sources/EasyPreference.swift
+++ Sources/EasyPreference.swift
@@ -8,35 +8,23 @@
 
 import Foundation
 
-public struct ValueChange<T> {
-    
-    public let oldValue: T
-    
-    public let newValue: T
-    
-    init(_ old: T, _ new: T) {
-        oldValue = old
-        newValue = new
-    }
-}
-
 public class EasyPreference: NSObject {
     
     let defaults: UserDefaults
     
-    var events = [String: Event]()
+    var events = [String: [Observing]]()
     
     public init(defaults: UserDefaults) {
         self.defaults = defaults
     }
     
-    @discardableResult public func subscribe<T: NSCoding>(key: PreferenceKey<T>, initial: Bool = false, using: @escaping (ValueChange<T>) -> Void) -> EventSubscription {
+    @discardableResult public func addObserver<T: NSCoding>(key: Key<T>, initial: Bool = false, using: @escaping (ValueChange<T>) -> Void) -> Observing {
         if !events.keys.contains(key.rawValue) {
             let option: NSKeyValueObservingOptions = initial ? [.old, .new, .initial] : [.old, .new]
             defaults.addObserver(self, forKeyPath: key.rawValue, options: option, context: nil)
-            events[key.rawValue] = Event()
+            events[key.rawValue] = []
         }
-        let subscription = EventSubscription() { old, new in
+        let subscription = Observing() { old, new in
             guard let oldData = old as? Data, let newData = new as? Data else {
                 return
             }
@@ -46,37 +34,36 @@ public class EasyPreference: NSObject {
             }
             using(ValueChange(oldValue, newValue))
         }
-        events[key.rawValue]?.add(subscription: subscription)
+        events[key.rawValue]?.append(subscription)
         return subscription
     }
     
-    @discardableResult public func subscribe<T>(key: PreferenceKey<T>, initial: Bool = false, using: @escaping (ValueChange<T>) -> Void) -> EventSubscription {
+    @discardableResult public func addObserver<T>(key: Key<T>, initial: Bool = false, using: @escaping (ValueChange<T>) -> Void) -> Observing {
         if !events.keys.contains(key.rawValue) {
             let option: NSKeyValueObservingOptions = initial ? [.old, .new, .initial] : [.old, .new]
             defaults.addObserver(self, forKeyPath: key.rawValue, options: option, context: nil)
-            events[key.rawValue] = Event()
+            events[key.rawValue] = []
         }
-        let subscription = EventSubscription() { old, new in
+        let subscription = Observing() { old, new in
             if let old = old as? T, let new = new as? T {
                 using(ValueChange(old, new))
             }
         }
-        events[key.rawValue]?.add(subscription: subscription)
+        events[key.rawValue]?.append(subscription)
         return subscription
     }
     
     public override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        guard let keyPath = keyPath,
-            let event = events[keyPath] else {
-                return
-        }
+        guard let keyPath = keyPath else { return }
         
-        guard let old = change?[.oldKey], let new = change?[.newKey] else {
-            return
-        }
+        events[keyPath] = observings.filter { $0.isValid }
+        guard let observings = events[keyPath] else { return }
         
-        event.notify(old, new)
-        if event.subscriptions.count == 0 {
+        guard let old = change?[.oldKey], let new = change?[.newKey] else { return }
+        
+        observings.forEach { $0.handler(old, new) }
+        
+        if observings.isEmpty {
             events.removeValue(forKey: keyPath)
         }
     }
@@ -86,62 +73,97 @@ public class EasyPreference: NSObject {
             defaults.removeObserver(self, forKeyPath: key)
         }
     }
-    
 }
 
 extension EasyPreference {
     
-    public subscript(_ key: PreferenceKey<String>) -> String? {
+    public struct ValueChange<T> {
+        
+        public let oldValue: T
+        
+        public let newValue: T
+        
+        init(_ old: T, _ new: T) {
+            oldValue = old
+            newValue = new
+        }
+    }
+}
+
+extension EasyPreference {
+    
+    public class Observing {
+        
+        typealias HandlerType = (_ old: Any, _ new: Any) -> Void
+        
+        var handler: HandlerType
+        
+        var isValid = true
+        
+        init(_ handler: @escaping HandlerType) {
+            self.handler = handler
+        }
+        
+        public func invalidate() {
+            handler = {_,_ in }
+            isValid = false
+        }
+    }
+}
+
+extension EasyPreference {
+    
+    public subscript(_ key: Key<String>) -> String? {
         get { return defaults.string(forKey: key.rawValue) }
         set { defaults.set(newValue, forKey: key.rawValue) }
     }
     
-    public subscript(_ key: PreferenceKey<[Any]>) -> [Any]? {
+    public subscript(_ key: Key<[Any]>) -> [Any]? {
         get { return defaults.array(forKey: key.rawValue) }
         set { defaults.set(newValue, forKey: key.rawValue) }
     }
     
-    public subscript(_ key: PreferenceKey<[String: Any]>) -> [String: Any]? {
+    public subscript(_ key: Key<[String: Any]>) -> [String: Any]? {
         get { return defaults.dictionary(forKey: key.rawValue) }
         set { defaults.set(newValue, forKey: key.rawValue) }
     }
     
-    public subscript(_ key: PreferenceKey<Data>) -> Data? {
+    public subscript(_ key: Key<Data>) -> Data? {
         get { return defaults.data(forKey: key.rawValue) }
         set { defaults.set(newValue, forKey: key.rawValue) }
     }
     
-    public subscript(_ key: PreferenceKey<[String]>) -> [String]? {
+    public subscript(_ key: Key<[String]>) -> [String]? {
         get { return defaults.stringArray(forKey: key.rawValue) }
         set { defaults.set(newValue, forKey: key.rawValue) }
     }
     
-    public subscript(_ key: PreferenceKey<Int>) -> Int {
+    public subscript(_ key: Key<Int>) -> Int {
         get { return defaults.integer(forKey: key.rawValue) }
         set { defaults.set(newValue, forKey: key.rawValue) }
     }
     
-    public subscript(_ key: PreferenceKey<Float>) -> Float {
+    public subscript(_ key: Key<Float>) -> Float {
         get { return defaults.float(forKey: key.rawValue) }
         set { defaults.set(newValue, forKey: key.rawValue) }
     }
     
-    public subscript(_ key: PreferenceKey<Double>) -> Double {
+    public subscript(_ key: Key<Double>) -> Double {
         get { return defaults.double(forKey: key.rawValue) }
         set { defaults.set(newValue, forKey: key.rawValue) }
     }
     
-    public subscript(_ key: PreferenceKey<Bool>) -> Bool {
+    public subscript(_ key: Key<Bool>) -> Bool {
         get { return defaults.bool(forKey: key.rawValue) }
         set { defaults.set(newValue, forKey: key.rawValue) }
     }
     
-    public subscript(_ key: PreferenceKey<URL>) -> URL? {
+    public subscript(_ key: Key<URL>) -> URL? {
         get { return defaults.url(forKey: key.rawValue) }
         set { defaults.set(newValue, forKey: key.rawValue) }
     }
     
-    public subscript(_ key: PreferenceKey<Any>) -> Any? {
+    public subscript(_ key: Key<Any>) -> Any? {
         get { return defaults.object(forKey: key.rawValue) }
         set { defaults.set(newValue, forKey: key.rawValue) }
     }
@@ -149,40 +171,44 @@ extension EasyPreference {
     // Generic Subscripts has implemented in Swift 4.
     // before that, use getter/setter instead.
     
-    public func unarchive<T: NSCoding>(for key: PreferenceKey<T>) -> T? {
+    public func unarchive<T: NSCoding>(for key: Key<T>) -> T? {
         guard let data = defaults.data(forKey: key.rawValue) else {
             return nil
         }
         return NSKeyedUnarchiver.unarchiveObject(with: data) as? T
     }
     
-    public func archive<T: NSCoding>(_ newValue: T, for key: PreferenceKey<T>) {
+    public func archive<T: NSCoding>(_ newValue: T, for key: Key<T>) {
         let data = NSKeyedArchiver.archivedData(withRootObject: newValue)
         defaults.set(data, forKey: key.rawValue)
     }
 }
 
-public class PreferenceKeys {}
+extension EasyPreference {
 
-public class PreferenceKey<T>: PreferenceKeys, RawRepresentable, Hashable, ExpressibleByStringLiteral {
-    
-    public let rawValue: String
-    
-    public var hashValue: Int {
-        return rawValue.hashValue
+    public class Keys {}
+
+    public class Key<T>: Keys, RawRepresentable, Hashable, ExpressibleByStringLiteral {
+        
+        public let rawValue: String
+        
+        public var hashValue: Int {
+            return rawValue.hashValue
+        }
+        
+        public init(_ rawValue: String) {
+            self.rawValue = rawValue
+        }
+        
+        public required convenience init(rawValue: String) {
+            self.init(rawValue)
+        }
+        
+        public required convenience init(stringLiteral value: String) {
+            self.init(value)
+        }
     }
     
-    public init(_ rawValue: String) {
-        self.rawValue = rawValue
-    }
-    
-    public required convenience init(rawValue: String) {
-        self.init(rawValue)
-    }
-    
-    public required convenience init(stringLiteral value: String) {
-        self.init(value)
-    }
 }
 
 extension ExpressibleByUnicodeScalarLiteral where Self: ExpressibleByStringLiteral, Self.StringLiteralType == String {
